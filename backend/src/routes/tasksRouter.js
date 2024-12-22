ffe865a305b7b7223674e813a8e72c3c6bf123e1
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-12-22 20:59:20 -0300

    feat: update routers with the controller

--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -1,25 +1,21 @@
 import express from 'express';
+import {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getTaskById,
+} from '../controllers/taskController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Lista de todas as tarefas' });
-});
+router.use(authMiddleware);
 
-router.post('/', (req, res) => {
-    const { title, description } = req.body;
-    res.json({ message: 'Tarefa criada com sucesso', task: { title, description } });
-});
-
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { title, description } = req.body;
-    res.json({ message: `Tarefa com ID ${id} atualizada com sucesso`, task: { title, description } });
-});
-  
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    res.json({ message: `Tarefa com ID ${id} deletada com sucesso` });
-});
+router.get('/', getAllTasks);
+router.get('/:id', getTaskById);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
 
 export default router;
--- backend/src/routes/usersRouter.js
+++ backend/src/routes/usersRouter.js
@@ -1,30 +1,19 @@
 import express from 'express';
+import {
+  getUserProfile,
+  registerUser,
+  loginUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Lista de todos os usuários' });
-});
-  
-router.post('/', (req, res) => {
-    const { name, email, role } = req.body;
-    res.json({ message: 'Usuário criado com sucesso', user: { name, email, role } });
-});
-  
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    res.json({ message: `Detalhes do usuário com ID ${id}` });
-});
-  
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, email, role } = req.body;
-    res.json({ message: `Usuário com ID ${id} atualizado com sucesso`, user: { name, email, role } });
-});
-  
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    res.json({ message: `Usuário com ID ${id} deletado com sucesso` });
-});
-  
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.use(authMiddleware);
+router.get('/profile', getUserProfile);
+router.put('/profile', updateUserProfile);
+
 export default router;
