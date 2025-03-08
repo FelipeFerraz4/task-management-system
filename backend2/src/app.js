16823d16017b99058569b3adc6457668b74a3b3d
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-03-08 00:34:29 -0300

    feat: created new backend structure

---
+++ backend2/src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const morgan = require("morgan");
+
+// const tourRouter = require("./routes/tourRoutes");
+// const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware 👋");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.get("/api/config", (req, res) => {
+    res.json({
+    port: process.env.PORT, 
+    environment: process.env.NODE_ENV,
+    });
+});
+
+// 3) ROUTES
+// app.use("/api/v1/tours", tourRouter);
+// app.use("/api/v1/users", userRouter);
+
+module.exports = app;
