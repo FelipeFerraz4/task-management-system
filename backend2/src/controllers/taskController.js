eb2e7422f9b21b885902fbbbd3463d3ba8ebd10b
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-03-18 23:05:03 -0300

    feat: created get function of taskController

---
+++ backend2/src/controllers/taskController.js
@@ -0,0 +1,37 @@
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const Task = require('../models/task');
+
+exports.getAllTasks = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Task.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const tasks = await features.query;
+
+  res.status(200).json({
+    status: 'sucess',
+    results: tasks.lenght,
+    data: {
+      tasks,
+    },
+  });
+});
+
+exports.getTask = catchAsync(async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
+
+  if (!task) {
+    return next(new AppError('No task found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      task,
+    },
+  });
+});
