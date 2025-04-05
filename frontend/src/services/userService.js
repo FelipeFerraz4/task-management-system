96396c7313c12e815b8bee4f312598ba88294861
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-05 17:27:46 -0300

    feat: created updateMe function

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,13 +3,17 @@ import axios from "axios";
 const API_URL = "http://localhost:3000/api/v1/users/me";
 
 const getMe = async () => {
-  const response = await axios.get(
-    API_URL,
-    {
-      withCredentials: true,
-    }
-  );
+  const response = await axios.get(API_URL, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
-export default getMe;
+const updateMe = async (params) => {
+  const response = await axios.patch(API_URL, params, {
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export default {getMe, updateMe};
