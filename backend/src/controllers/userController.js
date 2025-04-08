c42a9e43b4f2582bff0e6a48b5c67daef5129281
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-07 21:25:36 -0300

    feat: update in create user route

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -83,12 +83,26 @@ exports.getUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role || 'employee',
   });
-};
+
+  newUser.password = undefined;
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,7 +4,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post(
+  '/signup',
+  authController.protect,
+  authController.restrictTo('admin', 'rh'),
+  authController.signup,
+);
 router.post('/login', authController.login);
 
 router.post('/forgotpassword', authController.forgotPassword);
